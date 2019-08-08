a8c13ec89a412d0e711b0d41e76f6bdaec34d7c2
Nang Nguyen <nang.nguyen@evizi.com>
2019-08-08 10:28:06 +0700

    Add Module VIPER

---
+++ File Templates/Custom/Module VIPER.xctemplate/Universal/View/___FILEBASENAME___ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___
+//  ___COPYRIGHT___
+//
+
+import UIKit
+
+class ___FILEBASENAME___: UIViewController {
+    var presenter: ___VARIABLE_moduleName___ViewInput!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        presenter.viewDidLoad()
+    }
+}
+
+// MARK: ___VARIABLE_moduleName___ViewOutput
+extension ___VARIABLE_moduleName___ViewController: ___VARIABLE_moduleName___ViewOutput {
+    
+}
+
+// MARK: Button Action
+extension ___VARIABLE_moduleName___ViewController {
+    
+}
